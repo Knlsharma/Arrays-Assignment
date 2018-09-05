5db3e4f3952f16ec10f6d6ec057d98ebf673c48a
Kunal Sharma <knlsharma678@gmail.com>
2018-09-05 21:13:05 +0530

    added 2_d

---
+++ Max_2d.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Max_2d
+{
+  public static void main(String ...S) throws IOException
+  {  BufferedReader b = new BufferedReader (new InputStreamReader(System.in)); //Buffered reader class imported for reading from user and putting it to memory buffer
+      System.out.println("Enter the no. of arrays");   
+         int m = Integer.parseInt(b.readLine());    
+      System.out.println("Enter the length of each array.");   
+         int n = Integer.parseInt(b.readLine());//readline() is the method of class Buffered reader  which we impliment 
+     int ar [][] ,maxm = 0,maxn=0, i , j;
+     ar  = new int[m][n];
+      System.out.println("Enter the elements");
+     for(i=0 ;i<m; i++ )
+    {  for(j=0 ;j<n; j++ )
+     {    ar[i][j] = Integer.parseInt(b.readLine());
+        }
+    }    
+
+ for(i=0 ;i<m; i++ )
+    {  for(j=0 ;j<n; j++ )
+        { if(ar[i][j] > ar[maxm][maxn])
+          {  maxm = i;
+             maxn = j ; }
+            }
+        }
+    System.out.println("Max element is : " + ar[maxm][maxn] +".\nFound in array : "+(maxm+1)+".\nAt position : " +(maxn+1)+"." );
+    }
+}
